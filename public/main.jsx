a03e2e84bf3c841ec98ba85660c2b311266b9b19
Isaac Hess <isaachess@gmail.com>
2014-11-07 15:56:50 -0700

    V1 working

--- public/main.jsx
+++ public/main.jsx
@@ -1,20 +1,54 @@
-var HelloMessage = React.createClass({
-    render: function() {
-        return <div>Hello {this.props.name}</div>;
-    }
-});
+/* <Form/>
+ * <DisplayArea/>
+ *
+* */
 
-var CommentBox = React.createClass({
+var testSentence = "Hello world. This is a real sentence."
+var App = React.createClass({
+    getInitialState: function() {
+        return {data: testSentence}
+    },
+    handleNewWord: function(word) {
+        console.log('handleNewWord', word)
+        return this.setState({data: this.state.data + word})
+    },
     render: function() {
         return (
-            <div className="commentBox">
-                Hello, world! I am a CommentBox.
+            <div>
+                <SubmitForm submitFunc={this.handleNewWord} />
+                <DisplayArea sentence={this.state.data}/>
             </div>
-        );
+        )
     }
-});
+})
+
+var SubmitForm = React.createClass({
+    addWord: function(e) {
+        // TODO: Handle http post
+        e.preventDefault()
+        var word = this.refs.newWord.getDOMNode().value.trim()
+        console.log('addWord', word)
+        return this.props.submitFunc(word)
+    },
+    render: function() {
+        return (
+            <form onSubmit={this.addWord}>
+                <input type="text" ref="newWord" />
+            </form>
+        )
+    }
+})
+
+var DisplayArea = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.sentence}</div>
+        )
+    }
+})
+
 React.render(
-    <CommentBox />,
+    <App/>,
     document.getElementById('content')
 );
 
