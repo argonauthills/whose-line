1c49b0b651c4183eb57907aa02fb6b7fa6112762
Jason Hills <jasonahills@gmail.com>
2014-11-07 10:58:00 -0700

    initial commit

---
+++ server/main.js
@@ -0,0 +1,28 @@
+console.log("running server");
+
+var express = require("express");
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser')
+
+var pathToDist = "../dist";
+var pathToPublic = "../public";
+
+var comments = []
+
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
+app.use('/bower', express.static(path.join(__dirname, '..', 'bower_components')));
+
+app.post('/comments', function(req, res) {
+    comments.push(req.body)
+})
+
+app.get('/comments', function(req, res) {
+    res.send(comments)
+})
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.listen(1337);
