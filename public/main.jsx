84c44c457302f8cc2d0b3700679fb203af231545
Isaac Hess <isaachess@gmail.com>
2014-11-07 15:17:29 -0700

    Converted to 4 spaces

--- public/main.jsx
+++ public/main.jsx
@@ -1,21 +1,21 @@
 var HelloMessage = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
-  }
+    render: function() {
+        return <div>Hello {this.props.name}</div>;
+    }
 });
 
 var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className="commentBox">
+                Hello, world! I am a CommentBox.
+            </div>
+        );
+    }
 });
 React.render(
-  <CommentBox />,
-  document.getElementById('content')
+    <CommentBox />,
+    document.getElementById('content')
 );
 
 console.log("works!")
