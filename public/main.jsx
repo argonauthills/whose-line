c44aaf191d04215294f622ea564a79406b39c37a
Jason Hills <jasonahills@gmail.com>
2014-11-15 08:57:18 -0700

    refactors main.jsx; also makes google parsing a little more robust

--- public/main.jsx
+++ public/main.jsx
@@ -4,41 +4,39 @@
 */
 
 var App = React.createClass({
+    
     getInitialState: function() {
-        return {data: ""}
+        return {text: ""}
     },
+
     requestGoogleWord: function(phrase) {
-        var wordsToSend = lastFewWords(joinWords(this.state.data, phrase))
-        var encodedPhrase = encodeURIComponent(wordsToSend)
-        var url="http://suggestqueries.google.com/complete/search?client=chrome&q="+encodedPhrase
-        $.ajax({
-            url: url,
-            type: 'GET',
-            crossDomain: true,
-            dataType: 'jsonp',
-            success: function(rs) {
-                this.setState({data: joinWords(this.state.data, parseNextGoogleWord(rs, this.state.data))})
-            }.bind(this),
-            error: function(error) {
-                console.log('error',error)
-            }.bind(this),
-        })
+        askGoogleForWord(phrase)
+        .done(function(rs) {
+            console.log("google rs", rs)
+            var newWord = parseGoogleResponse(rs)
+            this.setState({text: updatedText(this.state.text, newWord)})
+        }.bind(this))
+        .fail(function (err) { throw new Error(err) })
     },
+
     reset: function() {
-        this.setState({data: ""})
+        this.setState({text: ""})
     },
+
     handleNewWord: function(word) {
-        this.setState({data: joinWords(this.state.data, word)})
-        console.log('handleNewWord', word)
+        var updated = updatedText(this.state.text, word)
+        console.log("last sentence", updated, lastSentence(updated))
+        this.setState({text: updated})
         if (word == "reset") return this.reset()
-        return this.requestGoogleWord(word)
+        else return this.requestGoogleWord(lastSentence(updated))
     },
+
     render: function() {
         return (
             <div className="app">
                 <div className="inner-border">
                     <div className="terminal-display">
-                        <DisplayArea sentence={this.state.data}/>
+                        <DisplayArea sentence={this.state.text}/>
                         <SubmitForm submitFunc={this.handleNewWord} />
                     </div>
                 </div>
@@ -48,9 +46,11 @@ var App = React.createClass({
 })
 
 var SubmitForm = React.createClass({
+    
     getInitialState: function() {
         return {spaces: "", currentInput: "&nbsp;", focused:true}
     },
+
     addWord: function(e) {
         // TODO: Handle http post
         e.preventDefault()
@@ -58,21 +58,25 @@ var SubmitForm = React.createClass({
         this.refs.newWord.getDOMNode().value = ''
         return this.props.submitFunc(word)
     },
+
     getWord: function() {
         return this.refs.newWord.getDOMNode().value.trim()
     },
+
     focus: function() {
         this.setState({focused:true})
         this.refs.newWord.getDOMNode().focus()
     },
+
     blur: function() {
         this.setState({focused:false})
     },
+
     checkPos: function(e) {
         var pos = e.target.selectionStart
-        var spaces = Array(pos + 1).join("&nbsp;")  //Hacky way to get array of spaces of a given length   
-        this.setState({spaces: spaces, currentInput: this.getWord()})
+        this.setState({spaces: spaces(pos + 1), currentInput: this.getWord()})
     },
+
     render: function() {
         var cx = React.addons.classSet
         var cursorClasses = cx({
@@ -107,25 +111,46 @@ React.render(
 
 
 
-function parseNextGoogleWord(response) {
-    console.log('response', response)
-    var oldLength = response[0].split(' ').length
-    var newWord = response[1][0].split(' ')[oldLength]
-    console.log('oldLength',oldLength,'newWord',newWord)
-    return newWord
+
+
+function lastSentence(text) {
+    var sentences = _.compact(text.split("."))
+    return sentences[sentences.length - 1]
 }
 
-function lastFewWords(string) {
-    //var splitWords = string.split(' ')
-    //var lastWords = splitWords.slice(Math.max(splitWords.length - 5, 0))
-    //var x = lastWords.join(' ')
-    //console.log('words sent', x)
-    //return x
-    console.log('string', string)
-    return string
+function joinWords(text, word) {
+    if (!text) return word
+    else return text + ' ' + word
 }
 
-function joinWords(phrase, word) {
-    if (!phrase) return word
-    else return phrase + ' ' + word
+function updatedText(text, newWord) {
+    if (!newWord) return text + ". "
+    else return joinWords(text, newWord)
 }
+
+function spaces(length) {
+    return Array(length).join("&nbsp;")  //Hacky way to get array of spaces of a given length  
+}
+
+function askGoogleForWord(str) {
+    var encodedStr = encodeURIComponent(str + " ")  // extra space so that we don't get parts of words, eg. "insidious" as a response for "in"
+    var url = "http://suggestqueries.google.com/complete/search?client=chrome&q="+encodedStr
+    return $.ajax({
+        url: url,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+    })
+}
+
+function parseGoogleResponse(rs) {
+    var oldPhrase = rs[0]
+    var newWordIndex = _.compact(oldPhrase.split(' ')).length
+    if (rs[1] && rs[1][0] && _.compact(rs[1][0].split(' '))[newWordIndex]) 
+        return _.compact(rs[1][0].split(' '))[newWordIndex]
+    else return null
+}
+
+
+
+
